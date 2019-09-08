bdc610a8a8b72be8556fa5e2a8bcb4ce5adb9098
Dean L Betty <deanlloyd64@gmail.com>
2019-09-08 18:33:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,3 +38,12 @@ def map_to_square(source_array)
   end
   new_array
 end
+
+def reduce_to_total(source_array, starting_point = 0)
+  i = 0
+  while i < source_array[i] do
+    starting_point = starting_point + source_array[i]
+    i += 1
+  end
+  starting_point
+end
