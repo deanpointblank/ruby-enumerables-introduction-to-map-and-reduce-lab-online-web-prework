fec23e8e9d7b8fa3a704bfbd2e90ccd9c40f0285
Dean L Betty <deanlloyd64@gmail.com>
2019-09-08 18:35:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -41,7 +41,7 @@ end
 
 def reduce_to_total(source_array, starting_point = 0)
   i = 0
-  while i < source_array[i] do
+  while i < source_array.length do
     starting_point = starting_point + source_array[i]
     i += 1
   end
