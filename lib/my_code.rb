2d24757c13c75fed312c4c2b13859f48967ed6cb
Dean L Betty <deanlloyd64@gmail.com>
2019-09-08 18:39:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,7 +51,7 @@ end
 def reduce_to_all_true(source_array)
   i = 0
   value = nil
-  while 1 < source_array.length do
+  while i < source_array.length do
     value = source_array[i - 1] && source_array [i]
     i += 1
   end
